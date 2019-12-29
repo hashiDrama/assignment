6a42d36b0e96567a853dccf5cf0fb90c81376eb0
Vikrant Saini <VikrantS@talentica-all.com>
2019-12-29 20:02:47 +0530

    beautifying the response

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def index():
 
 @app.route('/isTwoSidedPrime/<inputNumber>')
 def checkTwoSidedPrime(inputNumber):
-    return str(isTwoSidedPrime.checkTwoSidedPrime(inputNumber))
+    return {'result': isTwoSidedPrime.checkTwoSidedPrime(inputNumber)}
 if __name__ == "__main__":
     app.run(debug=True)
